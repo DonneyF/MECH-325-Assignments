3561e3db223389aee48e47b9575a6dae0eaa2171
Donney Fan <donney.fan@hotmail.com>
2018-10-01 00:45:09 -0700

    Testing four gear case

--- combinations_analysis.py
+++ combinations_analysis.py
@@ -3,39 +3,109 @@ import itertools
 
 gears = Gear()
 
-def compute_gear_combination(pinion1, gear1, pinion2, gear2):
+def compute_gear_permutation(pinion1, gear1, pinion2, gear2):
     gear_arr = [pinion1, gear1, pinion2, gear2]
+    fail = False
+    results = []
 
-    num_gears = 2 if pinion2 is None or gear2 is None else 4
+    # Compute for the first gear interaction
+    for i in range(0, 2):
+        bending_stress = gears.bending_stress(pinion1, gear1, None, None, i + 1)
+        safety_factor_bending = gears.safety_factor_bending(gear_arr[i], bending_stress)
 
-    for i in range(0, num_gears):
-        print("Gear {}:\n".format(i))
-        bending_stress = gears.bending_stress(pinion1, gear1, pinion2, gear2, i + 1)
-        safety_factor_bending = gears.safety_factor_bending(gear_arr[i - 1], bending_stress)
+        contact_stress = gears.contact_stress(pinion1, gear1, None, None, i + 1)
+        safety_factor_contact = gears.safety_factor_contact(pinion1, gear1, contact_stress)
 
-        contact_stress = gears.contact_stress(pinion1, gear1, pinion2, gear2, i + 1)
-        safety_factor_contact = gears.safety_factor_contact(gear_arr[i], gear_arr[i + 1], contact_stress)
-        cost = gears.interaction_cost(gear_arr[i], gear_arr[i + 1])
+        if safety_factor_contact < 2.2 or safety_factor_bending < 2.2:
+            failed = True
+            #print("FAILED")
+            break
 
-        print("Bending Stress: {}\nSafety Factor Bending: {} \nContact Stress: {}\nSafety Factor Contact: {}\n"
-              .format(bending_stress, safety_factor_bending, contact_stress, safety_factor_contact,cost))
+        gear = {
+            "bending_stress": bending_stress,
+            "safety_factor_bending": safety_factor_bending,
+            "contact_stress": contact_stress,
+            "safety_factor_contact": safety_factor_contact
+        }
+        results.append(gear)
+        # print("Gear {}:".format(i))
+        # print("Bending Stress: {}\nSafety Factor Bending: {} \nContact Stress: {}\nSafety Factor Contact: {}\n"
+        #       .format(bending_stress, safety_factor_bending, contact_stress, safety_factor_contact))
+        # print("====")
 
-        if num_gears <= 2:
-            cost = gears.interaction_cost(gear_arr[0], gear_arr[1])
+    # Compute for the second interaction
+    if pinion2 is not None and gear2 is not None:
+        for i in range(2, 4):
+            bending_stress = gears.bending_stress(pinion1, gear1, pinion2, gear2, i + 1)
+            safety_factor_bending = gears.safety_factor_bending(gear_arr[i], bending_stress)
+
+            contact_stress = gears.contact_stress(pinion1, gear1, pinion2, gear2, i + 1)
+            safety_factor_contact = gears.safety_factor_contact(pinion2, gear2, contact_stress)
+
+            if safety_factor_contact < 2.2 or safety_factor_bending < 2.2:
+                #print("FAILED")
+                fail = True
+                break
+
+            gear = {
+                "bending_stress": bending_stress,
+                "safety_factor_bending": safety_factor_bending,
+                "contact_stress": contact_stress,
+                "safety_factor_contact": safety_factor_contact
+            }
+            results.append(gear)
+            # print("Gear {}:".format(i))
+            # print("Bending Stress: {}\nSafety Factor Bending: {} \nContact Stress: {}\nSafety Factor Contact: {}\n"
+            #     .format(bending_stress, safety_factor_bending, contact_stress, safety_factor_contact))
+            # print("====")
+
+    if not fail:
+        for i, result in enumerate(results):
+            print("Gear: {}".format(i))
+            print("Bending Stress: {}\nSafety Factor Bending: {} \nContact Stress: {}\nSafety Factor Contact: {}\n"
+                 .format(result["bending_stress"], result["safety_factor_bending"], result["contact_stress"], result["safety_factor_contact"]))
+            print("====")
+
+        if len(results) == 2:
+            velocity = gears.power_screw_velocity(pinion1, gear1, None, None)
+            cost = gears.system_cost(pinion1, gear1, None, None)
+            performance = gears.performance_metric(pinion1, gear1, None, None)
         else:
-            cost = gears.interaction_cost(gear_arr[0], gear_arr[1]) + gears.interaction_cost(gear_arr[2], gear_arr[3])
+            velocity = gears.power_screw_velocity(pinion1, gear1, pinion2, gear2)
+            cost = gears.system_cost(pinion1, gear1, pinion2, gear2)
+            performance = gears.performance_metric(pinion1, gear1, pinion2, gear2)
 
-        print("Cost: {}".format(cost))
+        print("Power Screw Veloctity: {}\nSystem Cost: {}\nPerformance Metric: {}".format(velocity, cost, performance))
         print("=======================")
 
-gear_numbers = [gear["number"] for gear in gears.gears]
+def main():
+    combo = int(input("Two or Four Gear Combo? (2 or 4): "))
+    gear_numbers = [gear["number"] for gear in gears.gears]
 
-two_gear_combos = itertools.combinations(gear_numbers, 2)
-four_gear_combos = itertools.combinations(gear_numbers, 4)
+    if combo == 2:
+        two_gear_combos = itertools.permutations(gear_numbers, 2)
+        for combo in two_gear_combos:
+            gear1 = gears.get(combo[0])
+            gear2 = gears.get(combo[1])
+            e = gears.train_value(gear1, gear2, None, None)
 
-for combo in two_gear_combos:
-    gear1 = gears.get(combo[0])
-    gear2 = gears.get(combo[1])
-    e = gears.train_value(gear1, gear2, None, None)
-    if e <= 1.0/4.7:
-        compute_gear_combination(gear1, gear2, None, None)
+            # Minimum train value
+            if e <= 1.0/4.7:
+                print("Permutation: {}, {}".format(gear1["number"], gear2["number"]))
+                compute_gear_permutation(gear1, gear2, None, None)
+
+    elif combo == 4:
+        four_gear_combos = itertools.permutations(gear_numbers, 4)
+        for combo in four_gear_combos:
+            gear1 = gears.get(combo[0])
+            gear2 = gears.get(combo[1])
+            gear3 = gears.get(combo[2])
+            gear4 = gears.get(combo[3])
+            e = gears.train_value(gear1, gear2, gear3, gear4)
+
+            if e <= 1.0/5.0:
+                print("Permutation: {}, {}, {}, {}".format(gear1["number"], gear2["number"], gear3["number"], gear4["number"]))
+                compute_gear_permutation(gear1, gear2, gear3, gear4)
+
+if __name__ == "__main__":
+    main()
